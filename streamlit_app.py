176e54dcab4f20e6fc484bb28564ce0331264cc0
kelaslpk2026 <kelaslpk2026@gmail.com>
2026-05-19 11:28:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,8 @@ tambah=st.button("+")
 kurang=st.button("-")
 kali=st.button("x")
 tambah,kurang,kali=st.columns(3)
+if tambah:
+    st.subheader(f"{number1}+{number2}={number1+number2}")
 st.markdown("*Streamlit* is **really** ***cool***.")
 st.markdown('''
     :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
